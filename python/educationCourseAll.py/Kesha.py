f4979f7eb970ccd17c22ce5c6caa1068ab6f944e
YurgenSlavniy <108W108@gmail.com>
2021-04-17 22:09:16 +0300

    add 2 lesson python

---
+++ python/educationCourseAll.py/Kesha.py
@@ -0,0 +1,19 @@
+name = 'Кеша'
+age = 2
+period = 2
+
+print('Попугайчик')
+print(name)
+print('в')
+print(age)
+print('года')
+print('научился говорить')
+print('своё имя')
+print()
+print('Через')
+print(period)
+print('года')
+print('Он научился говорить')
+print('сколько ему лет ')
+print()
+print(age + period)
