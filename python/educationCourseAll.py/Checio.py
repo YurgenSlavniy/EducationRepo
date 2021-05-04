ce7dfc6fc926894b2802aadda87cea20802023d1
YurgenSlavniy <108W108@gmail.com>
2021-05-04 19:36:44 +0300

    add CHECIO 5 solutions Elementary 5/16

--- python/educationCourseAll.py/Checio.py
+++ python/educationCourseAll.py/Checio.py
@@ -68,3 +68,91 @@ def is_acceptable_password(password: str) -> bool:
 
 # Решение через lambda
 is_acceptable_password_lambda = lambda password: len(password) > 6
+
+
+# ЗАДАЧА 4
+#
+# Вам дано положительное целое число. Определите сколько цифр оно имеет.
+#
+# Входные данные: Положительное целое число
+#
+# Выходные данные: Целое число.
+#
+# Пример:
+#
+# number_length(10) == 2
+# number_length(0) == 1
+
+def number_length(a: int) -> int:
+    result = len(str(a))
+    return result
+
+
+# ЗАДАЧА 5
+#
+# Попробуйте выяснить какое количество нулей содержит данное число в конце.
+#
+# Входные данные: Положительное целое число (int).
+#
+# Выходные данные: Целое число (int).
+#
+# Пример:
+#
+# end_zeros(0) == 1
+# end_zeros(1) == 0
+# end_zeros(10) == 1
+# end_zeros(101) == 0
+
+def end_zeros(num: int) -> int:
+    num_str=str(num)
+    num_str=num_str[::-1]
+    count=0
+
+    for i in num_str:
+        if i=='0':
+            count+=1
+        else:
+            break
+    return count
+#--------------------
+def end_zeros(num: int) -> int:
+    return len(s := str(num)) - len(s.rstrip('0'))
+#--------------------
+end_zeros = lambda num: len(str(num)) - len(str(num).rstrip('0'))
+#--------------------
+import re
+def end_zeros(num: int) -> int:
+    return len(re.search('0*$', str(num)).group())
+#--------------------
+def end_zeros(number):
+    n = str(number)
+    return len(n) - len(n.strip('0'))
+
+def end_zeros(number):
+    number = str(number)
+    if number[-1:] != '0':
+        return 0
+    return 1 + end_zeros(number[:-1])
+
+def end_zeros(number):
+    if not number:
+       return 1
+    if not number % 10:
+       return 1 + end_zeros(number // 10)
+    return 0
+
+def end_zeros(number):
+    result = not number
+    while number and not number % 10:
+        number /= 10
+        result += 1
+    return result
+
+def end_zeros(number):
+    en = enumerate(str(number)[::-1])
+    return not number or next(i for i, x in en if x != '0')
+
+def end_zeros(number):
+    from itertools import takewhile
+    number = str(number)[::-1]
+    return len(list(takewhile(lambda x: x == '0', number)))
