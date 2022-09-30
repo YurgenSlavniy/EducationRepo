9ca347453cb8cb344848e90096e01dd70bc5c97d
YurgenSlavniy <108W108@gmail.com>
2022-09-30 12:01:31 +0300

    python examples

--- python/educationCourseAll.py/homework5example/mainHW5.py
+++
@@ -1,6 +0,0 @@
-import randomlist
-from folder import create_folders, delete_folders
-create_folders()
-delete_folders()
-print(randomlist.get_random([1, 3, 5, 7, 9]))
-print(randomlist.get_random([]))
--- python/educationCourseAll.py/homework5example/randomlist.py
+++
@@ -1,6 +0,0 @@
-import random
-
-def get_random(input_list):
-    if input_list:
-        result = random.choice(input_list)
-        return result
